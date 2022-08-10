dee404322519e5f65b5d5e7bd4f4f4582f8644e8
Sigalsha <sigal.shalit.manor@gmail.com>
2022-08-10 13:35:10 +0300

    mixed content issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   apiKey: 'DA89UW5pNtuvckCm87g94ZK6jn78zvHA',
   autocompleteApi:
     'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
-  currentWeatherApi: 'http://dataservice.accuweather.com/currentconditions/v1/',
+  currentWeatherApi:
+    'https://dataservice.accuweather.com/currentconditions/v1/',
   dailyForecastsApi:
     'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
 };
