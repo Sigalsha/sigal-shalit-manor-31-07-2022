ce0c628f684076314db6eb992ec1283ef8e1e952
Sigalsha <sigal.shalit.manor@gmail.com>
2022-08-10 13:28:51 +0300

    small fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   apiKey: 'DA89UW5pNtuvckCm87g94ZK6jn78zvHA',
   autocompleteApi:
-    'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+    'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
   currentWeatherApi: 'http://dataservice.accuweather.com/currentconditions/v1/',
   dailyForecastsApi:
-    'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+    'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
 };
 
 /*
